f42930fde3109bd69ee42c61a668ddb46e0bec2a
Gokul-Prabakran1 <165556566+Gokul-Prabakran1@users.noreply.github.com>
2024-11-16 17:08:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('CICD PROJECT USING GIT NPM SONARQUBE DOCKERIMAGE DOCKERCONTAINER DOCKERHUB  \n');
+  res.send('CICD PROJECT USING GIT NPM SONARQUBE DOCKERIMAGE TRIVY DOCKERCONTAINER DOCKERHUB  \n');
 });
 
 app.listen(port, () => {
